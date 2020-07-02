6cd264e6baff66bf92ad089eeac129565130f135
Alex Zima <Oleksandr_Zima@epam.com>
2020-07-03 00:32:43 +0300

    common HTML tasks table was replaced on the material table

--- src/app/views/tasks/tasks.component.ts
+++ src/app/views/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Task} from '../../model/Task';
 import {DataHandlerService} from '../../service/data-handler.service';
+import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'app-tasks',
@@ -9,6 +10,11 @@ import {DataHandlerService} from '../../service/data-handler.service';
 })
 export class TasksComponent implements OnInit {
 
+  displayedColumns: string[] = ['color', 'id', 'title', 'date', 'priority', 'category'];
+  dataSource: MatTableDataSource<Task>;
+  private readonly COMPLETED_TASK_COLOR = '#F8F9FA';
+  private readonly DEFAULT_TASK_COLOR = '#fff';
+
   tasks: Task[];
 
   constructor(private dataHandler: DataHandlerService) {
@@ -16,9 +22,26 @@ export class TasksComponent implements OnInit {
 
   ngOnInit(): void {
     this.dataHandler.tasksSubject.subscribe(tasks => this.tasks = tasks);
+    this.dataSource = new MatTableDataSource<Task>();
+    this.refreshTable();
   }
 
-  toggleTaskCompleted(task: Task) {
+  toggleTaskCompleted(task: Task): void {
     task.completed = !task.completed;
   }
+
+  getPriorityColor(task: Task): string {
+    if (task.completed) {
+      return this.COMPLETED_TASK_COLOR;
+    } else if (task.priority && task.priority.color) {
+      return task.priority.color;
+    } else {
+      return this.DEFAULT_TASK_COLOR;
+    }
+  }
+
+  private refreshTable(): void {
+    this.dataSource.data = this.tasks;
+  }
+
 }
